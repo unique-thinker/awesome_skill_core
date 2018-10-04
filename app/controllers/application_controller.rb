0a9d2a4131ee8eeb256d801c82040ed9405e5932
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-10-04 18:51:55 +0530

    Remove DeviseTokenAuth::Concerns::SetUserByToken

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
