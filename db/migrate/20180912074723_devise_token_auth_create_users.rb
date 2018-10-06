b719a8b38d0ebae40841b4224ea5f5135c41b355
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-10-06 18:09:36 +0530

    Add username unique constraint

--- db/migrate/20180912074723_devise_token_auth_create_users.rb
+++ db/migrate/20180912074723_devise_token_auth_create_users.rb
@@ -4,8 +4,8 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table(:users) do |t|
       ## User Info
-      t.string :username
-      t.string :email
+      t.string :username, index: { unique: true}
+      t.string :email, index: { unique: true}
       ## Required
       t.string :provider, null: false, default: 'email'
       t.string :uid, null: false, default: ''
@@ -14,7 +14,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
       t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
-      t.string   :reset_password_token
+      t.string   :reset_password_token, index: { unique: true}
       t.datetime :reset_password_sent_at
       t.boolean  :allow_password_change, default: false
 
@@ -29,7 +29,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
       t.string   :last_sign_in_ip
 
       ## Confirmable
-      t.string   :confirmation_token
+      t.string   :confirmation_token, index: { unique: true}
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
       t.string   :unconfirmed_email # Only if using reconfirmable
@@ -44,11 +44,10 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
 
       t.timestamps
     end
-
-    add_index :users, :email, unique: true
     add_index :users, %i[uid provider], unique: true
-    add_index :users, :reset_password_token, unique: true
-    add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :email, unique: true
+    # add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,       unique: true
   end
 end
