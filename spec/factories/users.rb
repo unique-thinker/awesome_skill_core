a769553ebe15eec0b6e6a6273dd2d6ddeaef541e
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-10-06 18:22:02 +0530

    Add People model association

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,16 @@
 
 FactoryBot.define do
   factory :user do
-    username { Faker::Internet.username }
+    username { Faker::Internet.username(5..8) }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
+
+    after(:build) do |u|
+      u.person = build(
+        :person,
+        profile_name: u.username,
+        owner: u
+      )
+    end
   end
 end
