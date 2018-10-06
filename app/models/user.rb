1273eae908d120706c2c2b15b93cf6ef5e1404b6
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-10-06 18:19:58 +0530

    Write more validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,34 @@ class User < ApplicationRecord
          :recoverable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  validates :username, :encrypted_password, presence: true
-  validates :username, uniqueness: {case_sensitive: false}
+  before_validation :strip_and_downcase_username
+
+  # Validations
+  validates :username, presence: true, uniqueness: true
+  validates_format_of :username, :with => /\A[A-Za-z0-9_]+\z/
+  validates :person, presence: true
+  validates_associated :person
+
+  # Association
+  has_one :person, inverse_of: :owner, foreign_key: :owner_id
+
+  def strip_and_downcase_username
+    if username.present?
+      username.strip!
+      username.downcase!
+    end
+  end
+
+  ###Helpers############
+  def self.build(opts={})
+    u = User.new(opts)
+    u.set_person
+    u
+  end
+
+  def set_person
+    person = Person.new
+    person.profile_name = self.username
+    self.person = person
+  end
 end
