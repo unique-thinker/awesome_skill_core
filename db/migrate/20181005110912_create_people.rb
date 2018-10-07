69733d32e4d50c38ebf89400f1d66bd508408c75
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-10-07 23:05:30 +0530

    Write person model validations rspec

--- db/migrate/20181005110912_create_people.rb
+++ db/migrate/20181005110912_create_people.rb
@@ -1,11 +1,11 @@
 class CreatePeople < ActiveRecord::Migration[5.2]
   def change
     create_table :people do |t|
-      t.string :profile_name, index: { unique: true}
+      t.string :profile_name, index: { unique: true }
       t.references :owner,
         references: :users,
         foreign_key: { to_table: :users },
-        index: { unique: true}
+        index: { unique: true }
 
       t.timestamps
     end
