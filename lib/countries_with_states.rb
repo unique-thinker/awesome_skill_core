02a6971f074a9a0cbfa6f939ca60fb841c072003
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-10-10 13:11:24 +0530

    Write Module for country states list

---
+++ lib/countries_with_states.rb
@@ -0,0 +1,28 @@
+module CountriesWithStates
+  # def countries_with_states(region='Asia', locale = 'en')
+  #   countries_with_states = []
+  #   ISO3166::Country.find_all_countries_by_region(region).map do |c|
+  #     h = {}
+  #     h[c.name] = c.alpha2
+  #     countries_with_states.push({
+  #       country: h,
+  #       states: c.subdivision_names_with_codes.to_h
+  #     })
+  #   end.sort
+  #   countries_with_states
+  # end
+
+  def country_with_states(country_code='IN')
+    country_with_states = []
+    country = Country.find_all_countries_by_alpha2(country_code)
+    country.map do |c|
+      h = {}
+      h[c.name] = c.alpha2
+      country_with_states.push({
+      country: h,
+      states: c.subdivision_names_with_codes.to_h
+    })
+    end.sort
+    country_with_states
+  end
+end
