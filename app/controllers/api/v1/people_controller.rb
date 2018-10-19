143ac2f455316d55c6b65ada86b9e1ff8f13b5a3
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-10-19 20:46:05 +0530

    Modify Country-with-states logic

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
-class PeopleController < Api::BaseController
+class Api::V1::PeopleController < Api::BaseController
+  before_action :authenticate_user!, except: %i[show]
 end
--- lib/countries_with_states.rb
+++ lib/countries_with_states.rb
@@ -13,15 +13,15 @@ module CountriesWithStates
   # end
 
   def country_with_states(country_code='IN')
-    country_with_states = []
+    country_with_states = {}
     country = Country.find_all_countries_by_alpha2(country_code)
     country.map do |c|
       h = {}
-      h[c.name] = c.alpha2
-      country_with_states.push({
-      country: h,
-      states: c.subdivision_names_with_codes.to_h
-    })
+      h[c.name] = {
+        id: c.alpha2,
+        states: c.subdivision_names_with_codes.map {|s| {name: s[0]}}
+      }
+      country_with_states.merge!(h)
     end.sort
     country_with_states
   end
