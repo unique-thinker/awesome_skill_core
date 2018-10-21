17ece7119b7b9f41074049a6c2d8f965fa76c808
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-10-21 12:05:05 +0530

    Create aspects migration

---
+++ db/migrate/20181020101424_create_aspects.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateAspects < ActiveRecord::Migration[5.2]
+  def change
+    create_table :aspects do |t|
+      t.string :name, null: false
+      t.references :user, null: false, index: true, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :aspects, %i[user_id name], unique: true
+  end
+end
