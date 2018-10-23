8602d31ae9a8eb3439c607b94759d5640350600c
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-10-23 09:30:39 +0530

    Create posts migration

---
+++ db/migrate/20181021103604_create_posts.rb
@@ -0,0 +1,19 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.boolean     :public, default: false, null: false
+      t.string      :guid, index: {unique: true}, null: false
+      t.text        :text
+      t.string      :type, limit: 40, null: false
+      t.references  :author,
+                    references:  :people,
+                    foreign_key: {to_table: :people},
+                    index: true,
+                    null: false
+
+      t.timestamps
+    end
+    add_index :posts, %i[id type]
+    add_index :posts, %i[id type created_at]
+  end
+end
