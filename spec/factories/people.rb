972da7c5016239465e91df486b87b685800a1ee4
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-10-23 09:28:07 +0530

    Write after build and create factory

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :person do
+  factory :person , aliases: %i[author] do
     profile_name { Faker::Internet.username }
+
+    after(:build) do |person|
+      person.profile = build(:profile, person: person)
+    end
+
+    after(:create) do |person|
+      person.profile.save
+    end
   end
 end
