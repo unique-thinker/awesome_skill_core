422186627730851e98171b71feb605c4c226f364
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-10-26 10:50:01 +0530

    Remove delegate

--- app/models/concerns/fields/author.rb
+++ app/models/concerns/fields/author.rb
@@ -8,8 +8,6 @@ module Fields
       model.class_eval do
         belongs_to :author, class_name: 'Person'
 
-        delegate :diaspora_handle, to: :author
-
         validates :author, presence: true
       end
     end
