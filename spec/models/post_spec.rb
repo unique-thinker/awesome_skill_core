3ac679ac1b1451bbbc2888c257ea44ebe182d8b8
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-10-26 11:02:38 +0530

    Write more Post model rspec and delete StatusMessage Rspec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,25 +4,11 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   let!(:user) { create(:user) }
-  let!(:status) { create(:status_message) }
 
+  it { is_expected.to respond_to(:public) }
+  it { is_expected.to respond_to(:guid) }
+  it { is_expected.to respond_to(:text) }
   it { should validate_uniqueness_of(:guid) }
-
-  describe 'validations' do
-    it 'validates uniqueness of guid and does not throw a db error' do
-      expect(FactoryBot.build(:status_message, guid: status.guid)).not_to be_valid
-    end
-  end
-
-  describe 'post_type' do
-    it 'returns the class constant' do
-      expect(status.type).to eq('StatusMessage')
-    end
-  end
-
-  describe '.params_initialize' do
-    it 'takes provider_display_name' do
-      expect(StatusMessage.params_initialize(status.attributes.merge(author: user.person)).text).to eq(status.text)
-    end
-  end
+  it { should belong_to(:postable) }
+  it { should validate_length_of(:text).is_at_most(65_535) }
 end
