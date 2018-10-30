d182bf74adad0afa62c983c2e1188a8e4752ebfb
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-10-30 09:49:32 +0530

    Create AspectVisibilty model factory and Write rspec

---
+++ spec/factories/aspect_visibilities.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :aspect_visibility do
+    aspect
+
+    factory :aspect_visibility_for_post do
+      association :shareable, factory: :post
+    end
+
+    factory :aspect_visibility_for_picture do
+      association :shareable, factory: :picture
+    end
+  end
+end
---
+++ spec/models/aspect_visibility_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe AspectVisibility, type: :model do
+  it { should belong_to(:aspect) }
+  it { should belong_to(:shareable) }
+
+  let(:post) { create(:post) }
+  let(:post_in_aspect) { create(:post_in_aspect) }
+  let(:aspect) { create(:aspect) }
+  let(:picture_with_same_id) { create(:picture, id: post_in_aspect.id) }
+
+  describe '.create' do
+    it 'creates object when attributes are fine' do
+      expect {
+        AspectVisibility.create(shareable: post, aspect: aspect)
+      }.to change(AspectVisibility, :count).by(1)
+    end
+
+    it 'doesn\'t allow duplicating objects' do
+      expect {
+        AspectVisibility
+          .create(shareable: post_in_aspect, aspect: post_in_aspect.aspects.first)
+          .save!
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'makes difference between shareable types' do
+      expect {
+        AspectVisibility.create(shareable: picture_with_same_id, aspect: post_in_aspect.aspects.first).save!
+      }.not_to raise_error
+    end
+  end
+end
