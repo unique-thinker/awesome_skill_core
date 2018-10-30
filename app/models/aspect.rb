c664056c207e9377b9d333eea8a9d1a74070dcfe
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-10-30 10:03:08 +0530

    Write has-many aspect-visibilities, posts and pictures associations

--- app/models/aspect.rb
+++ app/models/aspect.rb
@@ -3,6 +3,9 @@
 class Aspect < ApplicationRecord
   # Association
   belongs_to :user
+  has_many :aspect_visibilities, dependent: :destroy
+  has_many :posts, through: :aspect_visibilities, source: :shareable, source_type: 'Post'
+  has_many :pictures, through: :aspect_visibilities, source: :shareable, source_type: 'Picture'
 
   # Validations
   validates :name, presence: true, length: {maximum: 20}, null: false
@@ -11,5 +14,20 @@ class Aspect < ApplicationRecord
 
   before_create do
     self.order_id ||= Aspect.where(user_id: user_id).maximum(:order_id || 0).to_i + 1
+  end
+
+  def to_s
+    name
+  end
+
+  def << (shareable)
+    case shareable
+      when Post
+        self.posts << shareable
+      when Picture
+        self.pictures << shareable
+      else
+        raise "Unknown shareable type '#{shareable.class.base_class.to_s}'"
+    end
   end
 end
