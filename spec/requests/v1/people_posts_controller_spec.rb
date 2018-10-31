97b4b97b2eb7dd19aa027f056048455363729422
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-10-31 09:34:36 +0530

    Write Rspec for PeoplePost

--- spec/requests/v1/people_posts_controller_spec.rb
+++ spec/requests/v1/people_posts_controller_spec.rb
@@ -13,11 +13,8 @@ RSpec.describe Api::V1::PeoplePostsController, type: :request do
 
   let(:post_valid_params) {
     {
-    post_message: {text: people_post.text},
-    aspect_ids: [aspect_1.id.to_s],
-    image_attributes: [Rack::Test::UploadedFile.new(
-        Rails.root.join($fixtures_dir, 'picture.png').to_s, "image/png"
-      )]
+      post_message: {text: people_post.text},
+      aspect_ids: [aspect_1.id.to_s]
     }
   }
 
@@ -35,11 +32,80 @@ RSpec.describe Api::V1::PeoplePostsController, type: :request do
 
     describe 'POST /people/:person_id/people_posts' do
       context 'with valid params' do
-        before do
-          post create_post_path, params: post_valid_params, headers: api_headers(response.headers)
-        end
         it 'create a post' do
-          binding.pry
+          post create_post_path, params: post_valid_params, headers: api_headers(response.headers)
+          expect(response.status).to eq(201)
+        end
+      end
+
+      context 'with invalid params' do
+        it 'should not create post' do
+          post create_post_path,
+            params: post_valid_params.merge(post_message: {text: 'abcde12345' * 8000}),
+            headers: api_headers(response.headers)
+          expect(response.status).to eq(403)
+        end
+      end
+      context 'with aspect_ids' do
+        before do
+          @aspect_2 = user.aspects.create(name: 'my apsect')
+        end
+
+        it 'takes one aspect as array in aspect_ids' do
+          post create_post_path, params: post_valid_params, headers: api_headers(response.headers)
+          expect(response.status).to eq(201)
+          post = Post.find_by_text(people_post.text)
+          expect(post.aspect_visibilities.map(&:aspect)).to eq([aspect_1])
+        end
+
+        it 'takes one aspect as string in aspect_ids' do
+          post create_post_path, params: post_valid_params.merge(aspect_ids: aspect_1.id.to_s), headers: api_headers(response.headers)
+          expect(response.status).to eq(201)
+          post = Post.find_by_text(people_post.text)
+          expect(post.aspect_visibilities.map(&:aspect)).to eq([aspect_1])
+        end
+
+        it 'takes public as array in aspect_ids' do
+          post create_post_path, params: post_valid_params.merge(aspect_ids: ['public']), headers: api_headers(response.headers)
+          expect(response.status).to eq(201)
+          post = Post.find_by_text(people_post.text)
+          expect(post.public).to be_truthy
+        end
+
+        it 'takes public as string in aspect_ids' do
+          post create_post_path, params: post_valid_params.merge(aspect_ids: 'public'), headers: api_headers(response.headers)
+          expect(response.status).to eq(201)
+          post = Post.find_by_text(people_post.text)
+          expect(post.public).to be_truthy
+        end
+
+        it 'takes all_aspect as array in aspect_ids' do
+          post create_post_path, params: post_valid_params.merge(aspect_ids: ['all_aspects']), headers: api_headers(response.headers)
+          expect(response.status).to eq(201)
+          post = Post.find_by_text(people_post.text)
+          expect(post.aspect_visibilities.map(&:aspect)).to match_array(user.aspects)
+        end
+
+        it 'takes all_aspect as string in aspect_ids' do
+          post create_post_path, params: post_valid_params.merge(aspect_ids: 'all_aspects'), headers: api_headers(response.headers)
+          expect(response.status).to eq(201)
+          post = Post.find_by_text(people_post.text)
+          expect(post.aspect_visibilities.map(&:aspect)).to match_array(user.aspects)
+        end
+      end
+
+      context 'with pictures' do
+        before do
+          image_files = [Rack::Test::UploadedFile.new(
+              Rails.root.join($fixtures_dir, 'picture.png').to_s, "image/png"
+            )]
+          post_valid_params.merge(image_file: image_files)
+        end
+
+        it 'will post a picture without text' do
+          post_valid_params.delete :text
+          post create_post_path, params: post_valid_params, headers: api_headers(response.headers)
+          expect(response.status).to eq(201)
         end
       end
     end
