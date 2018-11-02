e150396af2d2e401e3e7e3e3485f4d6e716521ea
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-11-02 08:06:35 +0530

    Create Comments migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_29_072914) do
+ActiveRecord::Schema.define(version: 2018_11_01_045337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,19 @@ ActiveRecord::Schema.define(version: 2018_10_29_072914) do
     t.datetime "updated_at", null: false
     t.index ["user_id", "name"], name: "index_aspects_on_user_id_and_name", unique: true
     t.index ["user_id"], name: "index_aspects_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text", null: false
+    t.string "guid", null: false
+    t.bigint "author_id", null: false
+    t.string "commentable_type", null: false
+    t.bigint "commentable_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_person_id"
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_id_and_commentable_type"
+    t.index ["guid"], name: "index_comments_on_guid", unique: true
   end
 
   create_table "people", force: :cascade do |t|
@@ -131,6 +144,7 @@ ActiveRecord::Schema.define(version: 2018_10_29_072914) do
 
   add_foreign_key "aspect_visibilities", "aspects"
   add_foreign_key "aspects", "users"
+  add_foreign_key "comments", "people", column: "author_id", name: "comments_author_id_fk", on_delete: :cascade
   add_foreign_key "people", "users", column: "owner_id"
   add_foreign_key "profiles", "people"
 end
