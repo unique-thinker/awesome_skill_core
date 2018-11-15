a0e756896c7b420b5be3af387b36610aedd416e5
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-11-15 12:58:33 +0530

    Create and write vote, like and dislike model rspec

---
+++ spec/models/dislike_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Dislike, type: :model do
+  it "has a valid factory" do
+    expect(build(:dislike)).to be_valid
+  end
+end
---
+++ spec/models/like_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  it "has a valid factory" do
+    expect(build(:like)).to be_valid
+  end
+end
