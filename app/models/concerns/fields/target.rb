c06f6d5d2f4ce0c4af864ebb4820f8794d60b1cd
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-11-15 12:28:10 +0530

    Create target filed concern

---
+++ app/models/concerns/fields/target.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Fields
+  module Target
+    extend ActiveSupport::Concern
+
+    included do
+      belongs_to :target, polymorphic: true
+
+      validates :target_id, uniqueness: {scope: %i(target_type author_id)}
+    end
+  end
+end
