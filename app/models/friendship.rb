64adaad874549de6f85e6fe47d5731523e16ba3e
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-11-23 15:41:55 +0530

    Write friendship validations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,4 +4,22 @@ class Friendship < ApplicationRecord
   belongs_to :user, inverse_of: :friendships
   belongs_to :friend, class_name: 'Person', inverse_of: :friendships
   validates :friend_id, uniqueness: {scope: :user_id}
+  validate :not_self, on: :create
+  validate :not_friends, on: :create
+  validate :not_pending, on: :create
+
+  private
+
+  def not_self
+    errors.add(:friend, 'can\'t be equal to user') if user.person == friend
+  end
+
+  def not_friends
+    errors.add(:friend, 'is already added') if user.friends.include?(friend)
+  end
+
+  def not_pending
+    errors.add(:friend, 'already requested friendship') if user.friend_requests.include?(friend)
+  end
+
 end
