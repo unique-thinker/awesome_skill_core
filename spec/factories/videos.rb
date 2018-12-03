52717b576f4f77de3fcaad919f2a2b7713eccd66
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-12-03 16:37:59 +0530

    Create video factory

---
+++ spec/factories/videos.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :video do
+    title { Faker::Hipster.sentence }
+    description { Faker::Hipster.paragraph }
+    random_string { SecureRandom.hex(10) }
+    views_count { rand(1..100000)}
+    association :author, factory: :person
+    association :videoable, factory: :post
+
+    after(:build) do |p|
+      p.processed_video.store! Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'alexa_sings_girl_on_fire.mp4'), 'video/mp4')
+      p.update_remote_path
+    end
+  end
+end
