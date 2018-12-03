c4041baabc189ade0def383474439b07da0e7cc2
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-12-03 17:27:00 +0530

    Write Rspec for title, description, height, width, author_id columns

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -5,11 +5,15 @@ require 'rails_helper'
 RSpec.describe Picture, type: :model do
   it { is_expected.to respond_to(:public) }
   it { is_expected.to respond_to(:guid) }
-  it { is_expected.to respond_to(:text) }
+  it { is_expected.to respond_to(:title) }
+  it { is_expected.to respond_to(:description) }
   it { is_expected.to respond_to(:remote_image_path) }
   it { is_expected.to respond_to(:remote_image_name) }
   it { is_expected.to respond_to(:random_string) }
+  it { is_expected.to respond_to(:height) }
+  it { is_expected.to respond_to(:width) }
   it { is_expected.to respond_to(:processed_image) }
+  it { should belong_to(:author) }
   it { should belong_to(:imageable) }
 
   it 'has a valid factory' do
