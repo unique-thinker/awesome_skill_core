378071bdab395416b0d55846cb1e1def01757ea2
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-12-04 11:10:29 +0530

    Add ENUM values for kind and status column

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Friendship < ApplicationRecord
+  # ENUM
+  enum kind: { social_friend: 'social_friend', family: 'family' }
+  enum status: { pending: 'pending', accepted: 'accepted', declined: 'declined', blocked: 'blocked' }
+
   belongs_to :user, inverse_of: :friendships
   belongs_to :friend, class_name: 'Person', inverse_of: :friendships
   validates :friend_id, uniqueness: {scope: :user_id}
