361db4447ff6830d4b1da604ba810d2cde3681db
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-12-05 23:03:42 +0530

    Change fixture file name

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -11,7 +11,7 @@ FactoryBot.define do
 
     after(:build) do |p|
       p.processed_video.store! Rack::Test::UploadedFile.new(
-        Rails.root.join('spec', 'fixtures', 'alexa_sings_girl_on_fire.mp4'), 'video/mp4'
+        Rails.root.join('spec', 'fixtures', 'video.mp4'), 'video/mp4'
       )
       p.update_remote_path
     end
