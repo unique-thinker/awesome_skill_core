01cc9a5bd59a114916f1389a295077587ef47194
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-12-06 13:37:55 +0530

    Add has_many :videos association

--- app/serializers/api/v1/post_serializer.rb
+++ app/serializers/api/v1/post_serializer.rb
@@ -3,4 +3,5 @@
 class Api::V1::PostSerializer < Api::ApplicationSerializer
   attributes :public, :guid, :text, :postable_id, :postable_type, :created_at
   has_many :pictures, as: :imageable, if: proc {|post| post.pictures.any? }
+  has_many :videos, as: :videoable, if: proc {|post| post.videos.any? }
 end
