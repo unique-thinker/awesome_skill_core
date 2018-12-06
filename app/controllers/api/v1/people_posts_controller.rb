fd2ec9ef3ee308a3a6cad0ca0650d0b0bc98827c
shailendra pratap singh <shailendra17singgh@gmail.com>
2018-12-06 13:39:11 +0530

    Write a logic to store post videos and images by category

--- app/controllers/api/v1/people_posts_controller.rb
+++ app/controllers/api/v1/people_posts_controller.rb
@@ -7,15 +7,14 @@ class Api::V1::PeoplePostsController < Api::BaseController
   before_action :authenticate_user!
 
   def create
-    pictures = []
     post_params = normalize_params.merge!(user: current_user)
-    normalize_params.delete(:image_files).each do |image_file|
-      pic = PictureCreationService.call(post_params.slice(:public, :user).merge!(image_file: image_file))
-      pictures << pic
-    end
-    post = PostManager::PeoplePostCreationService.call(post_params.merge(pictures: pictures))
+    pictures = store_pictures(post_params)
+    videos = store_videos(post_params)
+    post = PostManager::PeoplePostCreationService.call(post_params.merge(pictures: pictures, videos: videos))
+    store_post_categories(normalize_params[:category_ids], post)
+
     if post.save
-      options = {include: [:pictures]}
+      options = {include: [:pictures, :videos]}
       render json: Api::V1::PostSerializer.new(post, options), status: :created
     else
       render json: {success: false, errors: resource_errors(post)}, status: :unprocessable_entity
@@ -42,13 +41,16 @@ class Api::V1::PeoplePostsController < Api::BaseController
     render json: {error: error.message}, status: :forbidden
   end
 
+  private
   def normalize_params
     params.permit(
       post_message: [:text]
     ).to_h.merge(
       aspect_ids:  normalize_aspect_ids,
+      category_ids: normalize_category_ids,
       public:      [*params[:aspect_ids]].first == 'public',
-      image_files: [*params[:image_files]].compact
+      image_files: [*params[:image_files]].compact,
+      video_files: [*params[:video_files]].compact
     )
   end
 
@@ -59,5 +61,50 @@ class Api::V1::PeoplePostsController < Api::BaseController
     else
       aspect_ids
     end
+  end
+
+  def normalize_category_ids
+    category_ids = [*params[:category_ids]]
+    if [*params[:aspect_ids]].first == 'public'
+      category_ids
+    else
+      []
+    end
+  end
+
+  def store_pictures(post_params)
+    pictures = []
+    return pictures if post_params[:image_files].empty?
+    post_params.delete(:image_files).each do |image_file|
+      pic = PictureCreationService.call(post_params.slice(:public, :user).merge!(image_file: image_file))
+      pictures << pic
+    end
+    pictures
+  end
+
+  def store_videos(post_params)
+    videos = []
+    return videos if post_params[:video_files].empty?
+    post_params.delete(:video_files).each do |video_file|
+      video = VideoCreationService.call(post_params.slice(:public, :user).merge!(video_file: video_file))
+      videos << video
+    end
+    videos
+  end
+
+  def store_post_categories(category_ids, post)
+    return if category_ids.empty?
+    if !post.pictures.empty?
+      category_ids.each do |id|
+        pic_category = Category.picture.find(id)
+        post.categorizations.build(category: pic_category)
+      end
+    elsif !post.videos.empty?
+      category_ids.each do |id|
+        video_category = Category.video.find(id)
+        post.categorizations.build(category: video_category)
+      end
+    end
+    rescue ActiveRecord::RecordNotFound => e
   end
 end
